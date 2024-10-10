8f34046f5c42d919fee331a35d84035a731ef2b2
Igor Candido <igorcr39@gmail.com>
2024-10-10 14:17:15 -0300

    test: Add unit and integration tests with new rule of username update

--- src/core/user/tests/application/integration/test_update_user.py
+++ src/core/user/tests/application/integration/test_update_user.py
@@ -1,7 +1,7 @@
 from unittest.mock import create_autospec
 from uuid import uuid4
 from src.core.user.application.use_cases.update_user import UpdateUser
-from src.core.user.application.use_cases.exceptions import UserNotFound
+from src.core.user.application.use_cases.exceptions import UserNotFound, UsernameUnavailable
 from src.core.user.infra.in_memory_user_repository import InMemoryUserRepository
 from src.core.user.domain.user_repository import UserRepository
 from src.core.user.domain.user import User
@@ -54,4 +54,51 @@ class TestUpdateUser:
 
         assert updated_user.name == "Teste"
         assert updated_user.qnt_room == 2
+    
+    def test_update_username_with_existed_username_should_error(self):
+        user1 = User(
+            id=uuid4(),
+            name="Teste", 
+            username="Teste123", 
+            email="test@ulife.com.br", 
+            password='123test*', 
+            )
+        
+        user2 = User(
+            id=uuid4(),
+            name="Testando", 
+            username="user_test", 
+            email="testando@ulife.com.br", 
+            password='123456test*', 
+            )
+
+        repository = InMemoryUserRepository(users=[user1, user2])
+
+        use_case = UpdateUser(repository=repository)
+
+        with pytest.raises(UsernameUnavailable):
+            use_case.execute(UpdateUser.Input(id=user1.id, username=user2.username))
+
+    def test_update_username_with_own_username_should_success(self):
+        user = User(
+            id=uuid4(),
+            name="Teste", 
+            username="Teste123", 
+            email="test@ulife.com.br", 
+            password='123test*', 
+            )
+        repository = InMemoryUserRepository(users=[user])
+
+        use_case = UpdateUser(repository=repository)
+        request = UpdateUser.Input(
+            id=user.id,
+            username="Teste123"
+            )
+        
+        use_case.execute(request)
+
+        assert user.username == "Teste123"
+        assert user.name == "Teste"
+
+
 
--- src/core/user/tests/application/unit/test_update_user.py
+++ src/core/user/tests/application/unit/test_update_user.py
@@ -1,7 +1,7 @@
 from unittest.mock import create_autospec
 from uuid import uuid4
 from src.core.user.application.use_cases.update_user import UpdateUser
-from src.core.user.application.use_cases.exceptions import UserNotFound
+from src.core.user.application.use_cases.exceptions import UserNotFound, UsernameUnavailable
 from src.core.user.domain.user_repository import UserRepository
 from src.core.user.domain.user import User
 import pytest
@@ -18,6 +18,8 @@ class TestUpdateUser:
             )
         mock_repository = create_autospec(UserRepository)
         mock_repository.get_by_id.return_value = user
+        mock_repository.get_by_username.return_value = None
+
 
         use_case = UpdateUser(repository=mock_repository)
         request = UpdateUser.Input(
@@ -31,7 +33,7 @@ class TestUpdateUser:
         assert user.name == "Teste"
         mock_repository.update.assert_called_once_with(user)
 
-    def test_update_qnt_room(self):
+    def test_update_user_qnt_room(self):
         user = User(
             id=uuid4(),
             name="Teste", 
@@ -41,6 +43,7 @@ class TestUpdateUser:
             )
         mock_repository = create_autospec(UserRepository)
         mock_repository.get_by_id.return_value = user
+        mock_repository.get_by_username.return_value = user
 
         use_case = UpdateUser(repository=mock_repository)
         request = UpdateUser.Input(
@@ -53,4 +56,55 @@ class TestUpdateUser:
         assert user.name == "Teste"
         assert user.qnt_room == 2
         mock_repository.update.assert_called_once_with(user) 
+    
+    def test_update_username_with_existed_username_should_error(self):
+        user1 = User(
+            id=uuid4(),
+            name="Teste", 
+            username="Teste123", 
+            email="test@ulife.com.br", 
+            password='123test*', 
+            )
+        
+        user2 = User(
+            id=uuid4(),
+            name="Testando", 
+            username="user_test", 
+            email="testando@ulife.com.br", 
+            password='123456test*', 
+            )
+
+        mock_repository = create_autospec(UserRepository)
+        mock_repository.get_by_id.return_value = user1
+        mock_repository.get_by_username.return_value = user2
+
+        use_case = UpdateUser(repository=mock_repository)
+
+        with pytest.raises(UsernameUnavailable):
+            use_case.execute(UpdateUser.Input(id=user1.id, username=user2.username))
+
+    def test_update_username_with_own_username_should_success(self):
+        user = User(
+            id=uuid4(),
+            name="Teste", 
+            username="Teste123", 
+            email="test@ulife.com.br", 
+            password='123test*', 
+            )
+        mock_repository = create_autospec(UserRepository)
+        mock_repository.get_by_id.return_value = user
+        mock_repository.get_by_username.return_value = user
+
+
+        use_case = UpdateUser(repository=mock_repository)
+        request = UpdateUser.Input(
+            id=user.id,
+            username="Teste123"
+            )
+        
+        use_case.execute(request)
+
+        assert user.username == "Teste123"
+        assert user.name == "Teste"
+        mock_repository.update.assert_called_once_with(user)
 
