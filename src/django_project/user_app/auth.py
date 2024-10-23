9b3113b50592434f1f452473bc96608ac96f5dab
Igor Candido <igorcr.dev@gmail.com>
2024-10-23 08:14:31 -0300

    chore: Remove unused file

--- src/django_project/user_app/auth.py
+++
@@ -1,9 +0,0 @@
-from src.django_project.user_app.models import User
-from django.contrib.auth.hashers import check_password
-
-def authenticate(email, raw_password):
-    try:
-        user = User.objects.get(email=email)
-        return check_password(raw_password, user.password), user
-    except User.DoesNotExist:
-        return None, None
--- src/django_project/user_app/views.py
+++ src/django_project/user_app/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_201_CREATED, HTTP_204_NO_CONTENT
-from rest_framework_simplejwt.tokens import RefreshToken
-from src.django_project.user_app.auth import authenticate
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from src.django_project.user_app.serializers import CreateUserRequestSerializer, CreateUserResponseSerializer,  DeleteUserRequestSerializer, UpdatePartialUserRequestSerializer
 from src.django_project.user_app.repository import DjangoORMUserRepository
 from src.core.user.application.use_cases.create_user import CreateUser
