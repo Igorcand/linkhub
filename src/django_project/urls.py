c4512223628ed6eedb3b61ec0c17200259d0e5f4
Igor Candido Rodrigues <87720535+Igorcand@users.noreply.github.com>
2024-11-18 20:54:29 -0300

    Update urls.py

--- src/django_project/urls.py
+++ src/django_project/urls.py
@@ -50,6 +50,7 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
+   generator_class=BothHttpAndHttpsSchemaGenerator,
 )
 
 router = DefaultRouter()
